af1d900f5d913338bc21e9dd7198a5d6740251f8
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-06-10 18:03:57 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Calendar } from './Calendar';
+export { default as Kanban } from './Kanban';
+export { default as BarChart } from './BarChart';
