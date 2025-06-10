a8ce5f08694cb63f502f1d0525e8d685a7f2abf5
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-06-10 17:58:12 +0530

    Create themesetting.js

---
+++ src/components/themesetting.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThemeSettings = () => {
+  return (
+    <div className="fixed right-0 bottom-0 p-2 bg-blue-100 text-black">
+      Theme Settings Panel (To be implemented)
+    </div>
+  );
+};
+
+export default ThemeSettings;
