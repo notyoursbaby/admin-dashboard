80f3e09f411d9c3755aa4e65c635b5cf2688b766
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-06-10 18:01:16 +0530

    Create calendar.jsx

---
+++ src/pages/calendar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
+
+const Calendar = () => {
+  return (
+    <div className="p-8">
+      <h2 className="mb-4 text-lg font-bold">Calendar</h2>
+      <CalendarComponent></CalendarComponent>
+    </div>
+  );
+};
+
+export default Calendar;
