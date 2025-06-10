c543171de361fec9de19834b2ec63603c48ccc66
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-06-10 17:56:08 +0530

    Create navbar.jsx

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-4 bg-white shadow-md">
+      <div>Admin Dashboard</div>
+    </div>
+  );
+};
+
+export default Navbar;
